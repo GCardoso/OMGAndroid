297ef571fc603836ed9034b218293bd2c3ce2302
Guilherme <gcardoso18@gmail.com>
2015-07-08 17:37:43 -0300

    Mudança no arquivo de string para corrigir os nomes

--- app/src/main/java/com/example/guilhermecardoso/omgandroid/MainActivity.java
+++ app/src/main/java/com/example/guilhermecardoso/omgandroid/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.File;
@@ -21,25 +20,17 @@ import java.util.Date;
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     TextView mainTextView;
     Button mainButton;
-    EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 1. Access the TextView defined in layout XML
-// and then set its text
-        mainTextView = (TextView) findViewById(R.id.main_textview);
-        mainTextView.setText("Click for Photo!");
-
         // 2. Access the Button defined in layout XML
 // and listen for it here
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
 
-        // 3. Access the EditText defined in layout XML
-        mainEditText = (EditText) findViewById(R.id.main_edittext);
 
     }
 
