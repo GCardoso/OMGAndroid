049576b42b352cd39abc73d6438f51836a22dfaa
Guilherme <gcardoso18@gmail.com>
2015-08-31 16:13:10 -0300

    Melhoras nos comentários e organização

--- app/src/main/java/Services/FeatureDetectorAlgorithms.java
+++ app/src/main/java/Services/FeatureDetectorAlgorithms.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import org.opencv.android.Utils;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.CvException;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfDMatch;
@@ -20,7 +19,6 @@ import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.Features2d;
 import org.opencv.features2d.KeyPoint;
-import org.opencv.highgui.Highgui;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -125,8 +123,7 @@ public class FeatureDetectorAlgorithms {
             }
         }
 
-        //Log.i(TAG, "Max = " + maxDist + " Min = " + minDist);
-
+        //Adding only matches that has no more than 3 times the minimum distance
         LinkedList<DMatch> goodMatches = new LinkedList<DMatch>();
         for (int i = 0; i < descriptors1.rows(); i++){
             if (matcheslist.get(i).distance < 3 * minDist){
@@ -134,14 +131,16 @@ public class FeatureDetectorAlgorithms {
             }
         }
 
-        MatOfDMatch gm = new MatOfDMatch();
-        gm.fromList(goodMatches);
+        MatOfDMatch goodMatchesMat = new MatOfDMatch();
+        goodMatchesMat.fromList(goodMatches);
 
         //feature and connection colors
         Scalar RED = new Scalar(255,0,0);
         Scalar GREEN = new Scalar(0,255,0);
 
-        //now find the homography matrix
+        //now find the homography matrix, we need 2 lists, in this case
+        //will be form both images, but when used to recognize patterns, one is the train image
+        //and the other is the scene to be recognized
         List<KeyPoint> obj_keypont = keypoints_object.toList();
         List<KeyPoint> scene_keypont = keypoints_scene.toList();
 
@@ -153,6 +152,7 @@ public class FeatureDetectorAlgorithms {
             scene_pointsList.addLast(scene_keypont.get(goodMatches.get(i).trainIdx).pt);
         }
 
+        //Now is needed to convert to matOfPoint2f so we can use the finHomography function
         MatOfPoint2f obj = new MatOfPoint2f();
         obj.fromList(obj_pointsList);
         MatOfPoint2f scene = new MatOfPoint2f();
@@ -172,11 +172,7 @@ public class FeatureDetectorAlgorithms {
             }
 
 
-        Mat obj_corners = new Mat(4,1, CvType.CV_32FC2);
-        Mat scene_corners = new Mat(4,1,CvType.CV_32FC2);
-
-
-        //Cleaning not good matches
+        //Taking out the outliners, using the Mat was modified by the findHomography function
         Mat outputImg = new Mat();
         List<KeyPoint> listOfGoodKeypointsObj = keypoints_object.toList();
         List<KeyPoint> listOfGoodKeypointsScene = keypoints_scene.toList();
@@ -199,22 +195,12 @@ public class FeatureDetectorAlgorithms {
         }
 
         //output image
-        //this will draw all matches, works fine
-        //Features2d.drawMatches(img_object, keypoints_object, img_scene, keypoints_scene, gm,
-        //        outputImg, GREEN, RED, drawnMatches, Features2d.NOT_DRAW_SINGLE_POINTS);
-        //Features2d.drawMatches(img_object,keypoints_object,img_scene,keypoints_scene,gm,outputImg,GREEN,RED,new MatOfByte(inliners),Features2d.NOT_DRAW_SINGLE_POINTS);
         MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
         keypoints1.fromList(listOfGoodKeypointsObj);
         MatOfKeyPoint keypoints2 = new MatOfKeyPoint();
         keypoints2.fromList(listOfGoodKeypointsScene);
-        //Features2d.drawMatches(firstPath, keypoints1, secondPath, keypoints2, gm, outputImg, GREEN, RED, new MatOfByte(inliners), Features2d.NOT_DRAW_SINGLE_POINTS);
 
-        Features2d.drawMatches(firstPath, keypoints1, secondPath, keypoints2, gm, outputImg, GREEN, RED, new MatOfByte(), Features2d.NOT_DRAW_SINGLE_POINTS);
-
-//        Core.line(img_object, new Point(), new Point(), new Scalar(0, 255, 0), 4);
-//        Core.line(img_object, new Point(), new Point(), new Scalar(0,255,0), 4);
-//        Core.line(img_object, new Point(), new Point(), new Scalar(0, 255, 0), 4);
-//        Core.line(img_object, new Point(), new Point(), new Scalar(0, 255, 0), 4);
+        Features2d.drawMatches(firstPath, keypoints1, secondPath, keypoints2, goodMatchesMat, outputImg, GREEN, RED, new MatOfByte(), Features2d.NOT_DRAW_SINGLE_POINTS);
 
         Bitmap imageMatched = Bitmap.createBitmap(outputImg.cols(), outputImg.rows(), Bitmap.Config.RGB_565);//need to save bitmap
         Utils.matToBitmap(outputImg, imageMatched);
@@ -241,116 +227,6 @@ public class FeatureDetectorAlgorithms {
         return null;
     }
 
-//    public static Bitmap ORB(String firstPath, String secondPath){
-//        FeatureDetector detector = FeatureDetector.create(FeatureDetector.ORB);
-//        DescriptorExtractor descriptor = DescriptorExtractor.create(DescriptorExtractor.ORB);;
-//        DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE_HAMMING);
-//
-//
-//        //first image
-//        Mat img1 = Highgui.imread(firstPath);
-//        Mat descriptors1 = new Mat();
-//        MatOfKeyPoint keypoints1 = new MatOfKeyPoint();
-//
-//        detector.detect(img1, keypoints1);
-//        descriptor.compute(img1, keypoints1, descriptors1);
-//
-//        //second image
-//        Mat img2 = Highgui.imread(secondPath);
-//        Mat descriptors2 = new Mat();
-//        MatOfKeyPoint keypoints2 = new MatOfKeyPoint();
-//
-//        detector.detect(img2, keypoints2);
-//        descriptor.compute(img2, keypoints2, descriptors2);
-//
-//        //matcher should include 2 different image's descriptors
-//        //Also making 2 matchers to filter the matches
-//        MatOfDMatch matches12 = new MatOfDMatch();
-//        MatOfDMatch matches21 = new MatOfDMatch();
-//        matcher.match(descriptors1, descriptors2, matches12);
-//        matcher.match(descriptors2, descriptors1, matches21);
-//
-//        //Needs matches in DMatch format
-//        DMatch[] matchesConverted12 = matches12.toArray();
-//        DMatch[] matchesConverted21 = matches21.toArray();
-//        ArrayList<DMatch> filteredMatches12 = new ArrayList<DMatch>();
-//
-//        //Now need to convert the MatOfKeyPoint
-//        MatOfPoint2f keypointsConverted1 = convertMatOfKeypoint2MatOfPoint2f(keypoints1);
-//        MatOfPoint2f keypointsConverted2 = convertMatOfKeypoint2MatOfPoint2f(keypoints2);
-//
-//        Log.i(TAG,keypointsConverted1.toString());
-//        Log.i(TAG,keypointsConverted2.toString());
-//
-////        //Filtering matches, cross check
-////        for (int i = 0; i < matchesConverted12.length; i++){
-////            DMatch forward = matchesConverted12[i];
-////            DMatch backward = matchesConverted21[forward.trainIdx];
-////            if( backward.trainIdx == forward.queryIdx )
-////                filteredMatches12.add(forward);
-////        }
-//
-////        Collections.sort(filteredMatches12, new Comparator<DMatch>() {
-////            @Override
-////            public int compare(DMatch o1, DMatch o2) {
-////                if (o1.distance < o2.distance)
-////                    return -1;
-////                if (o1.distance > o2.distance)
-////                    return 1;
-////                return 0;
-////            }
-////        });
-////        int min = Math.min(matchesConverted12.length, matchesConverted21.length);
-////        if(filteredMatches12.size() > min){
-////            ArrayList<DMatch> aux = new ArrayList<DMatch>();
-////            for (int i = 0; i < min; i++){
-////                aux.add(filteredMatches12.get(i));
-////            }
-////            filteredMatches12 = aux;
-////        }
-//
-//        DMatch matc[] = new DMatch[filteredMatches12.size()];
-//        for(int i = 0; i < filteredMatches12.size();i++){
-//            matc[i] = filteredMatches12.get(i);
-//        }
-//
-//        //MatOfDMatch matches = new MatOfDMatch(matc);
-//        matches =
-//        //Find now the Fundamental Matrix
-//        Log.i(TAG,"keypointsConverted1 size = " + keypointsConverted1.toArray().length);
-//        Log.i(TAG,"keypointsConverted2 size = " + keypointsConverted2.toArray().length);
-//        //Mat fundamentalMatrix = Calib3d.findFundamentalMat(keypointsConverted1, keypointsConverted2);
-//
-//
-//        //Conversao do MatofDMatch para MatofPoint2f
-//        MatOfPoint2f prevPts = getMatOfPoint2fFromDMatchesTrain(matches,keypoints1);
-//        MatOfPoint2f nextPts = getMatOfPoint2fFromDMatchesQuery(matches, keypoints2);
-//
-//        //Calculo da Matriz Fundamental
-//        Mat fundamental_matrix = Calib3d.findFundamentalMat(
-//                nextPts, prevPts, Calib3d.FM_RANSAC, 3, 0.99);
-//
-//        //Calib3d.correctMatches(fundamental_matrix,keypoints1,keypoints2,keypoints1,keypoints2);
-//
-//        //feature and connection colors
-//        Scalar RED = new Scalar(255,0,0);
-//        Scalar GREEN = new Scalar(0,255,0);
-//
-//        //output image
-//        Mat outputImg = new Mat();
-//        MatOfByte drawnMatches = new MatOfByte();
-//
-//        //outputImg = fundamental_matrix;
-//
-//        //this will draw all matches, works fine
-//        Features2d.drawMatches(img1, keypoints1, img2, keypoints2, matches,
-//                outputImg, GREEN, RED, drawnMatches, Features2d.NOT_DRAW_SINGLE_POINTS);
-//
-//        Bitmap imageMatched = Bitmap.createBitmap(outputImg.cols(), outputImg.rows(), Bitmap.Config.RGB_565);//need to save bitmap
-//        Utils.matToBitmap(outputImg, imageMatched);
-//        return imageMatched;
-//    }
-
     static private MatOfPoint2f getMatOfPoint2fFromDMatchesTrain(MatOfDMatch matches2,
                                                                  MatOfKeyPoint prevKP2) {
         DMatch dm[] = matches2.toArray();
@@ -376,7 +252,5 @@ public class FeatureDetectorAlgorithms {
         }
         return new MatOfPoint2f(lp2.toArray(new Point[0]));
     }
-
-
-
+    
 }
