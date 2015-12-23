a52b54ab478cff68cf2c0ffcb4fe41f1b6f49484
Guilherme Cardoso <gcardoso18@gmail.com>
2015-12-22 21:14:09 -0300

    Correção de typos, e também agora a classe Main virou keyPointFinder

---
+++ app/src/main/java/Services/CameraConfiguration.java
@@ -0,0 +1,16 @@
+package Services;
+
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Size;
+
+import java.util.LinkedList;
+
+/**
+ * Created by guilhermegomes on 16/12/15.
+ */
+public class CameraConfiguration {
+
+
+}
